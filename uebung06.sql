4b0c2bac597fb89c33e9a0b1967d519961349903
ProofTyler <m1991-daniel@hotmail.de>
2016-05-19 10:13:01 +0200

    bla

--- uebung06.sql
+++ uebung06.sql
@@ -11,16 +11,16 @@ select Titel, Jahr, Name, Vorname
   
 -- LIT13
 select Name,Vorname, count(*) AS Anzahl
-from autor join BA using (AId)
-		   join Buch using (BId)
-group by BId 
+from autor join BA using (AId)		   
+group by Name, Vorname
 having Anzahl > 2
 order by Name,Vorname;
 
 -- LIT14
 select BId, Titel, ISBN, count(*) AS Anzahl_Autoren
 from Buch join BA using(BId)
-group by BId;
+group by Titel,ISBN,BId
+having Anzahl_Autoren > 1;
 
 -- LIT15
 select Verlag, Jahr, Titel, BId, ISBN, Name
@@ -30,18 +30,20 @@ where Verlag in ('Springer','Oldenbourg','Vieweg','Teubner')
 order by Verlag,Jahr,Titel;
 
 -- LIT16
-select Verlag from Buch
+select distinct Verlag from Buch
 where ort LIKE 'München'
 order by Verlag;
 
 -- LIT17
-select Name, Titel, Verlag, Jahr, Anmerkung
+select Name, Titel, Verlag, Jahr, Schlagwort, Anmerkung
 from buch join BA using(BId)
 		  join Autor using(AId)
 		  join BS using(BId)
 		  join Sachverz using(SId)
-where Schlagwort in ('Datenbanken','Datenbanksystem')
-and Jahr between 1995 and 2010
+where Jahr between 1995 and 2010
+and BId in (select Bid from BS join sachverz using (sid) 
+			where Schlagwort in ('Datenbanken','Datenbanksystem')
+            group by Bid having count(Sid) > 1)
 order by Jahr, Name;
 
 -- LIT18
@@ -59,6 +61,10 @@ select count(*) from
 	 having MIND_EIN_SCHLAGWORT =0
 	) 
     BS;
+
+select count(BId) from buch left outer join BS using (Bid)
+where SID is null;
+
 
 -- LIT20
 -- Überprüfung durch Äüßerer Verbund??
